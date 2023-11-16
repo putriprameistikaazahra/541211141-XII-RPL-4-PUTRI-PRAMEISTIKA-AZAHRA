9cbb7b3c5f3f2e72bba35b35c0111cbdae7f501a
putriprameistikaazahra <147579469+putriprameistikaazahra@users.noreply.github.com>
2023-11-16 13:12:17 +0700

    Add files via upload

---
+++ router/users.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+
+const usercontroller = require('../controllers/users')
+
+router.get('/users', usercontroller.index)
+
+router.get('/user/:id', usercontroller.show)
+
+router.post('/user', usercontroller.store)
+
+router.put('/user/:id', usercontroller.update)
+
+router.delete('/user/:id', usercontroller.destroy)
+
+module.exports = router
